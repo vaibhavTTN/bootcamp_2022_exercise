3a7069936be601cd42cc882aebe21dc613b9fbfc
vaibhav kamal <vaibhav.kamal@tothenew.com>
2022-10-02 23:21:34 +0530

    updated on java 9-17 exercise

--- Java_9-17_exercise/Java/src/Questions/Question11/Question11.java
+++ Java_9-17_exercise/Java/src/Questions/Question11/Question11.java
@@ -1,5 +1,7 @@
 package Questions.Question11;
 
+import java.util.Objects;
+
 /*
     Q11. use record to create an immutable represent of student(name, id, age) and use its  constructor for
     initialization, equals to compare 2 students methods.
@@ -7,8 +9,22 @@ package Questions.Question11;
  */
 record Student(String name,int id,int age) {
     static int count = 0;
+
     Student{
         count++;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id && age == student.age && Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id, age);
     }
 }
 
@@ -25,6 +41,11 @@ public class Question11 {
         System.out.println("Name :: "+ obj2.name());
         System.out.println("Age :: "+ obj2.age());
         System.out.println("Number of Object been created :: "+Student.count);
+        System.out.println();
+        System.out.println("Comparing two Objects :: "+ obj.equals(obj2));
+        System.out.println("HashCode Of Obj :: "+ obj.hashCode());
+        System.out.println("HashCode Of Obj2 :: "+ obj2.hashCode());
+
         // we will not be able change the value of the Obj
     }
 }
@@ -42,4 +63,7 @@ public class Question11 {
         Age :: 21
         Number of Object been created :: 2
 
+        Comparing two Objects :: false
+        HashCode Of Obj :: -1965008564
+        HashCode Of Obj2 :: -2131543587
  */
